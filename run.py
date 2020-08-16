0d70d7877bf9e9066086a34c83504ad6c35dfe02
anteCedens <jaksa.juric@yahoo.com.hr>
2020-08-16 22:54:49 +0000

    Added extra comments to code so far

--- run.py
+++ run.py
@@ -2,7 +2,9 @@
 from flask import Flask #Capital "F" indicates a class name
 
 #Create an instance of the Flask class
+#The first argument of the Flask class is the name of the applications module - our package
 app = Flask(__name__) #Convention is to name variables "app"
+# Since we're just using a single module, we can use"__name__" which is a built-in Python variable. Flask needs this so that it knows where to look for templates and static files
 
 #Use the "route" decorator to tell Flask what URL should trigger the function that follows
 @app.route("/")
