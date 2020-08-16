6f466c09c1d22084161417bc0798b5d4c3413d67
anteCedens <jaksa.juric@yahoo.com.hr>
2020-08-16 21:46:19 +0000

    Adding an instance of the Flask class, a route decorator and an index function, and comments

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-from flask import Flask
+# Import Flask class from flask
+from flask import Flask #Capital "F" indicates a class name
+
+#Create an instance of the Flask class
+app = Flask(__name__) #Convention is to name variables "app"
+
+#Use the "route" decorator to tell Flask what URL should trigger the function that follows
+@app.route("/")
+#Define a function, named "index"
+def index():
+    return "Hello, World"
